8dc3c9ec79e7af373b7fab7ee10dcdd2821e6e12
Simran Tyagi <tyagisimran938@gmail.com>
2024-12-03 16:28:47 +0530

    Initial commit

---
+++ lib/payments.ts
@@ -0,0 +1,3 @@
+export const monthlyPlanId: string = "price_1QQrMzSA6ZNS8y6AypkFyMrt";
+export const yearlyPlanId: string = "price_1QQrNaSA6ZNS8y6AKelO1SCi";
+export const maxFreeProjects: number = 3;
